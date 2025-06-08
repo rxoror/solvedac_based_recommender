6f24ab12197a568a64152309e7fec8bccd60e32b
rxoror <hoyun767@gmail.com>
2025-06-08 15:43:06 +0900

    문제 제목 반환 함수 추가

--- utils/requestApi.py
+++ utils/requestApi.py
@@ -42,3 +42,23 @@ def get_user_rating(handle):
     rating = data.get("rating")
 
     return rating # 해당 유저의 레이팅 수치를 반환함
+
+# 문제의 번호를 받아서, 문제 제목을 반환
+def get_problem_title(problem_num):
+    url = "https://solved.ac/api/v3/problem/show"
+
+    querystring = {"problemId":{problem_num}}
+
+    headers = {
+        "x-solvedac-language": "",
+        "Accept": "application/json"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    data = response.json()
+    data: dict = response.json()
+
+    title = data.get("titleKo")
+
+    return title
